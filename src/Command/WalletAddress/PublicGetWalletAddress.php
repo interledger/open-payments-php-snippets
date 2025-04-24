294b8adcd7afa7182bd13e5ac27c539eb82c372c
Adrian Boros <adiboros@hotmail.com>
2025-04-24 10:57:25 +0300

    update wallet calls and added block comments for documentation

--- src/Command/WalletAddress/PublicGetWalletAddress.php
+++ src/Command/WalletAddress/PublicGetWalletAddress.php
@@ -6,8 +6,18 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+//@! start chunk 1 | title=Import dependencies
 use OpenPayments\AuthClient;
 use OpenPayments\Config\Config;
+//@! end chunk 1
+
+/**
+ * Class PublicGetWalletAddress
+ * @package App\Command\WalletAddress
+ *
+ * This command is used to get a wallet address.
+ * It outputs the wallet address object.
+ */
 
 class PublicGetWalletAddress extends Command
 {
@@ -16,8 +26,8 @@ class PublicGetWalletAddress extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Outputs a friendly greeting.')
-            ->setHelp('This command allows you to output a greeting message...')
+            ->setDescription('This command is used to get a wallet address.')
+            ->setHelp('This command outputs the wallet address object.')
             ->addArgument(
                 'WALLET_ADDRESS',
                 InputArgument::OPTIONAL,
@@ -29,14 +39,21 @@ class PublicGetWalletAddress extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $WALLET_ADDRESS = $input->getArgument('WALLET_ADDRESS');
+
+        //@! start chunk 2 | title=Initialize Open Payments client
         $config = new Config($WALLET_ADDRESS);
         $opClient = new AuthClient($config);
+        //@! end chunk 2
 
+        //@! start chunk 3 | title=Get wallet address
         $wallet = $opClient->walletAddress()->get([
             'url' => $config->getWalletAddressUrl()
         ]);
+        //@! end chunk 3
 
-        $output->writeln('WALLET ADDRESS:<br><pre>'.print_r($wallet, true).'</pre>');
+        //@! start chunk 4 | title=Output wallet address
+        $output->writeln('WALLET ADDRESS '.print_r($wallet, true));
+        //@! end chunk 4
 
         return Command::SUCCESS;
     }
--- src/Command/WalletAddress/PublicGetWalletAddressKeys.php
+++ src/Command/WalletAddress/PublicGetWalletAddressKeys.php
@@ -6,8 +6,18 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+//@! start chunk 1 | title=Import dependencies
 use OpenPayments\AuthClient;
 use OpenPayments\Config\Config;
+//@! end chunk 1
+
+/**
+ * Class PublicGetWalletAddressKeys
+ * @package App\Command\WalletAddress
+ *
+ * This command is used to get the keys of a wallet address.
+ * It outputs a list of keys objects.
+ */
 
 class PublicGetWalletAddressKeys extends Command
 {
@@ -16,8 +26,8 @@ class PublicGetWalletAddressKeys extends Command
     protected function configure(): void
     {
         $this
-            ->setDescription('Outputs a friendly greeting.')
-            ->setHelp('This command allows you to output a greeting message...')
+            ->setDescription('This command is used to get the keys of a wallet address.')
+            ->setHelp('This command outputs a list of keys objects')
             ->addArgument(
                 'WALLET_ADDRESS',
                 InputArgument::OPTIONAL,
@@ -29,15 +39,22 @@ class PublicGetWalletAddressKeys extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $WALLET_ADDRESS = $input->getArgument('WALLET_ADDRESS');
+
+        //@! start chunk 2 | title=Initialize Open Payments client
         $config = new Config($WALLET_ADDRESS);
         $opClient = new AuthClient($config);
+        //@! end chunk 2
 
-        $wallet = $opClient->walletAddress()->getKeys([
+        //@! start chunk 3 | title=Get wallet address keys
+        $walletKeys = $opClient->walletAddress()->getKeys([
             'url' => $config->getWalletAddressUrl()
         ]);
+        //@! end chunk 3
 
-        $output->writeln('WALLET ADDRESS KEYS:<br><pre>'.print_r($wallet, true).'</pre>');
-
+        //@! start chunk 4 | title=Output wallet address keys
+        $output->writeln('WALLET ADDRESS KEYS: '.print_r($walletKeys, true));
+        //@! end chunk 4
+       
         return Command::SUCCESS;
     }
 }
